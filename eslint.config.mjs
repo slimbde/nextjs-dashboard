f7feb6244201e230926f34346fd26199c25db58e
slimbde <slimbde@gmail.com>
2025-02-01 13:33:53 +0500

    style/add-css-modules-plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 /* eslint-disable import/no-anonymous-default-export */
-import prettier from "eslint-plugin-prettier";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import prettier from 'eslint-plugin-prettier';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+import cssModules from 'eslint-plugin-css-modules';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,14 +15,28 @@ const compat = new FlatCompat({
 });
 
 export default [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'prettier',
+    'plugin:css-modules/recommended',
+    'plugin:prettier/recommended',
+  ),
   {
     plugins: {
       prettier,
+      'css-modules': cssModules,
     },
 
     rules: {
-      "prettier/prettier": "error",
+      'prettier/prettier': [
+        'warn',
+        {
+          singleQuote: true,
+          jsxSingleQuote: true,
+        },
+      ],
+      'css-modules/no-unused-class': 'warn',
     },
   },
 ];
