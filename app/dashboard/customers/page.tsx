10e40eda02dc4752b8698dc649002ab11aa4879a
slimbde <slimbde@gmail.com>
2025-08-24 20:10:08 +0500

    parallel-server-data-fetching

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
+import {
+  fetchCardData,
+  fetchLatestInvoices,
+  fetchRevenue,
+} from '@/app/lib/data';
+
+export default async function Page() {
+  const latestInvoicesData = fetchLatestInvoices();
+  const revenueData = fetchRevenue();
+  const cardData = fetchCardData();
+
+  const [invoices, revenue, card] = await Promise.all([
+    latestInvoicesData,
+    revenueData,
+    cardData,
+  ]);
+
   return <p>Customers Page</p>;
 }
